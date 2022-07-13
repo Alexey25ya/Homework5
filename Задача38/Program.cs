c7a5917ff07bf77e8fd66b64bee48f2efcd033ee
Alexey <alexey25ya@mail.ru>
2022-07-13 22:40:03 +0300

    ред

--- Задача38/Program.cs
+++ Задача38/Program.cs
@@ -1,25 +1,22 @@
 ﻿// Задайте массив вещественных чисел. 
 //Найдите разницу между максимальным и минимальным элементов массива.
 
-// Некорректные условия:
-// 1)Вещественные числа не использовал - в контексте задачи они не нужны
-// 2)Разницу понял как разность
 
-int[] getArray(int n)
+double[] getArray(int n)
 {
-    int[] arr = new int[n];
+    double[] arr = new double[n];
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(0, 11);
+        arr[i] = Convert.ToDouble(new Random().Next(-100, 101)/10.0);
     }
     return arr;
 }
 
-int[] array = getArray(10);
+double[] array = getArray(10);
 
-int getMin(int[] array1)
+double getMin(double[] array1)
 {
-    int min = array1[0];
+    double min = array1[0];
     for (int j = 0; j < array1.Length; j++)
     {
         if (min > array1[j])
@@ -30,9 +27,9 @@ int getMin(int[] array1)
     return min;
 }
 
-int getMax(int[] array2)
+double getMax(double[] array2)
 {
-    int max = array2[0];
+    double max = array2[0];
     for (int l = 0; l < array2.Length; l++)
     {
         if (max < array2[l])
